cf8bcc6513a554ac759463f18b65e902799a7775
LapereIlse <ilse.lapere@student.howest.be>
2018-11-04 11:53:34 +0100

    Azure Function - PostRegistration

---
+++ GarbageApp/Model/Registration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarbageApp.Model
+{
+    public class Registration
+    {
+        public Guid GarbageId { get; set; }
+        public string Description { get; set; }
+        public string Street { get; set; }
+        public string CategoryId { get; set; }
+        public Guid CityId { get; set; }
+        public int Amount { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public Guid PersonId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ GarbageApp/PostRegistration.cs
@@ -0,0 +1,70 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using GarbageApp.Model;
+using System.Data.SqlClient;
+
+namespace GarbageApp
+{
+    public static class PostRegistration
+    {
+        [FunctionName("PostRegistration")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "postregistration")] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                // Gegevens toekennen aan extra variabelen
+                Guid registrationId = Guid.NewGuid();
+                DateTime date = DateTime.Now;
+
+                // Connectie maken met de body
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+                // JSON in body opvragen
+                Registration registration = JsonConvert.DeserializeObject<Registration>(requestBody);
+
+                // Extra variabelen toekennen aan JSON
+                registration.GarbageId = registrationId;
+                registration.Date = date;
+
+                // Connectie maken met de database
+                using (SqlConnection connection = new SqlConnection(Environment.GetEnvironmentVariable("ConnectionString")))
+                {
+                    await connection.OpenAsync();
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        command.CommandText = "INSERT INTO Garbage VALUES (@GarbageId, @Description, @CategoryId, @Street, @CityId, @Amount, @Latitude, @Longitude, @PersonId, @Date);";
+
+                        command.Parameters.AddWithValue("@GarbageId", registration.GarbageId);
+                        command.Parameters.AddWithValue("@Description", registration.Description);
+                        command.Parameters.AddWithValue("@CategoryId", registration.CategoryId);
+                        command.Parameters.AddWithValue("@Street", registration.Street);
+                        command.Parameters.AddWithValue("@CityId", registration.CityId);
+                        command.Parameters.AddWithValue("@Amount", registration.Amount);
+                        command.Parameters.AddWithValue("@Latitude", registration.Latitude);
+                        command.Parameters.AddWithValue("@Longitude", registration.Longitude);
+                        command.Parameters.AddWithValue("@PersonId", registration.PersonId);
+                        command.Parameters.AddWithValue("@Date", registration.Date);
+
+                        await command.ExecuteNonQueryAsync();
+                    }
+                }
+                return new OkObjectResult(registration);
+            }
+
+            catch (Exception ex)
+            {
+                return new OkObjectResult(ex);
+            }           
+        }
+    }
+}
